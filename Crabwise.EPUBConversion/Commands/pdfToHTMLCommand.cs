3eff3e5a405ffc5cf847d43b272f4d3abacbb3f9
Tom Grochowicz <tgrochowicz@crabwisestudios.com>
2010-03-31 19:33:24 -0400

    adding source

---
+++ Crabwise.EPUBConversion/Commands/pdfToHTMLCommand.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crabwise.EPUBConversion
+{
+    using Crabwise.CommandWrap.Library;
+    using System.Collections.ObjectModel;
+
+    [CommandSyntax("pdftohtml.exe")]
+    public class pdfToHTMLCommand : Command
+    {
+
+        [ParameterSyntax("-f {arg}")]
+        public int? FirstPageToConvert
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-l {arg}")]
+        public int? LastPageToConvert
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-q")]
+        public bool? OmitMessagesAndErrors
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-h")]
+        public bool? PrintUserInformation
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-help")]
+        public bool? PrintUsageInformation
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-p")]
+        public bool? ExchangePDFLinksByHTML
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-c")]
+        public bool? GenerateComplexDocument
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-i")]
+        public bool? IgnoreImages
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-noframes")]
+        public bool? GenerateNoFrames
+        {
+            get;
+            set;
+        }
+        
+        [ParameterSyntax("-stdout")]
+        public bool? WriteToStdOut
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-zoom {arg}")]
+        public double? Zoom
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-xml")]
+        public bool? OutputForXMLPostProcessing
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-hidden")]
+        public bool? OutputHiddenText
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-nomerge")]
+        public bool? DoNotMergeParagraphs
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-enc {arg}")]
+        public String OutputTextEncodingName
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-dev {arg}")]
+        public String OutputDeviceName
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-v")]
+        public bool? PrintCopyrightInfo
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-opw {arg}")]
+        public String OwnerPassword
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("-upw {arg}")]
+        public String UserPassword
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("{arg}", Priority=3, Required=true)]
+        public String PDFFileLocation
+        {
+            get;
+            set;
+        }
+
+        [ParameterSyntax("{arg}", Priority=2)]
+        public String OutputFileLocation
+        {
+            get;
+            set;
+        }
+    }
+
+}
---
+++ Crabwise.EPUBConversion/HtmlConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crabwise.EPUBConversion
+{
+    public static class HtmlConversion
+    {
+        public static string GenerateHTMLFromFile(string pdfFilePath)
+        {
+            pdfToHTMLCommand cmd = new pdfToHTMLCommand();
+            cmd.PDFFileLocation = pdfFilePath;
+            cmd.WriteToStdOut = true;
+            cmd.ExchangePDFLinksByHTML = true;
+            Crabwise.CommandWrap.Library.CommandStartInfo startinfo = new Crabwise.CommandWrap.Library.CommandStartInfo(new System.Diagnostics.ProcessStartInfo());
+            startinfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory();
+            startinfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            cmd.Execute(startinfo);
+            return cmd.StandardOutput;
+        }
+    }
+}
