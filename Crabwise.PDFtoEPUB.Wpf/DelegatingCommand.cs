69bf7973cd3b7d77460526ea509b50e21629fae7
Greg Curtis <gcurtis@crabwisestudios.com>
2010-04-01 00:41:47 -0400

    Created binding for buttons

---
+++ Crabwise.PDFtoEPUB.Wpf/DelegatingCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Crabwise.PDFtoEPUB.Wpf
+{
+    public sealed class DelegatingCommand : ICommand
+    {
+        readonly Func<object, bool> canExecuteDelegate;
+        readonly Action<object> executeDelegate;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DelegatingCommand(Func<object, bool> canExecuteDelegate, Action<object> executeDelegate)
+        {
+            this.canExecuteDelegate = canExecuteDelegate;
+            this.executeDelegate = executeDelegate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteDelegate(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            executeDelegate(parameter);
+        }
+    }
+}
--- Crabwise.PDFtoEPUB.Wpf/MainWindow.xaml.cs
+++ Crabwise.PDFtoEPUB.Wpf/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
 
 namespace Crabwise.PDFtoEPUB.Wpf
 {
@@ -19,9 +21,82 @@ namespace Crabwise.PDFtoEPUB.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ICommand addButtonCommand;
+        private readonly ICommand browseButtonCommand;
+        private readonly ICommand removeButtonCommand;
+        private readonly ObservableCollection<object> selectedItems = new ObservableCollection<object>();
+
+        public ICommand AddButtonCommand
+        {
+            get
+            {
+                return addButtonCommand;
+            }
+        }
+
+        public ICommand BrowseButtonCommand
+        {
+            get
+            {
+                return browseButtonCommand;
+            }
+        }
+
+        public ICommand RemoveButtonCommand
+        {
+            get
+            {
+                return removeButtonCommand;
+            }
+        }
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); this.DataContext = this;
+
+            this.addButtonCommand = new DelegatingCommand(this.CanExecuteAdd, this.ExecuteAdd);
+            this.browseButtonCommand = new DelegatingCommand(this.CanExecuteBrowse, this.ExecuteBrowse);
+            this.removeButtonCommand = new DelegatingCommand(this.CanExecuteRemove, this.ExecuteRemove);
+        }
+
+        private bool CanExecuteAdd(object parameter)
+        {
+            return true;
+        }
+
+        private bool CanExecuteBrowse(object parameter)
+        {
+            return true;
+        }
+
+        private bool CanExecuteRemove(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteAdd(object parameter)
+        {
+            this.ShowFileDialog("Add PDFs", true);
+        }
+
+        private void ExecuteBrowse(object parameter)
+        {
+            this.ShowFileDialog("Output Folder", true);
+        }
+
+        private void ExecuteRemove(object parameter)
+        {
+        }
+
+        private void ShowFileDialog(string caption, bool folderPicker)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                CheckPathExists = true,
+                Title = caption
+            };
+
+            openFileDialog.ShowDialog();
         }
     }
 }
